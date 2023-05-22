67e78d375f93d5bcb6c393e6d7370ab4b8858d1c
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-05-22 16:52:32 -0400

    fix: file housekeeping

--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 
 ## Author: Jerry Barrows-Fitzgerald
 
-Note: This project is not yet completed. I turned in some starter code, but did not have time
-to complete this assignment. I'm hoping I'll be able to catch up tomorrow morning.
+To run: python web-scraper.py
--- page_parser.py
+++ page_parser.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 def parse(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    citations = soup.select("a[href='https://en.wikipedia.org/wiki/Wikipedia:Citation_needed']")
-    citation_context = {}
-    citation_num = 0
+    citations = soup.find_all(title="Wikipedia:Citation needed")
+    citation_context = set()
+    citation_num = len(citations)
     for citation in citations:
         citation_num += 1
-        citation_context.add(citation.parent)
+        citation_context.add(citation.parent.parent.parent)
     return [citation_num, citation_context]
